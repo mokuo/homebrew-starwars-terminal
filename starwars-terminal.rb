dfb3cb07e4cc01afe5914beadb42a3860e37312a
mokuo <tennis10988.yk@gmail.com>
2021-01-02 18:48:21 +0900

    Create Formula file by `brew create https://github.com/mokuo/starwars-terminal/archive/0.tar.gz` .

---
+++ starwars-terminal.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class StarwarsTerminal < Formula
+  desc "StarWars terminal themes inspired at Pokemon-Terminal. https://github.com/LazoCoder/Pokemon-Terminal"
+  homepage ""
+  url "https://github.com/mokuo/starwars-terminal/archive/0.tar.gz"
+  sha256 "a278784e852884d71b53cc4e5def3c4ec7952f94a4a69d5fee6d21d3555f3210"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test starwars-terminal`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
